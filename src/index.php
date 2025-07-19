9b3c8afd52b2c4c02d2f02a21ac1cf3c0429b7eb
daniil_linevych21 <dan4224rit@gmail.com>
2025-07-19 19:25:55 +0300

    initial project

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<?php
+require 'vendor/autoload.php';
+
+$redis = new Predis\Client([
+    'scheme' => 'tcp',
+    'host' => 'redis-test',
+    'port' => 6379,
+]);
+
+$redis->set('msg', 'Hello Predis');
+echo "Redis says: " . $redis->get('msg') . "<br>";
+
+$mysqli = new mysqli('mysql-db', 'appuser', 'secret', 'appdb');
+if ($mysqli->connect_error) {
+    die("MySQL connection failed: " . $mysqli->connect_error);
+}
+echo "MySQL connection successful!";
